97ab5fe602944ccb693d45968e6f7ce6d4beacc2
Benzébuth <benjamin.colin.dev@gmail.com>
2021-09-26 12:11:57 +0200

    minor alias maj

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,7 +7,7 @@ for index ({1..9}) alias "$index"="cd +${index}"; unset index
 alias v="$EDITOR"
 alias vim="$EDITOR"
 
-alias ls="lsd"
-alias ll="lsd -la"
+alias ls="lsd --group-dirs first"
+alias ll="lsd -la --group-dirs first"
 
 alias gcc="clang"
